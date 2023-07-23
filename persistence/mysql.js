3d614cdb12c934716c35db08d0f1081fed93dbc1
LucasMdct <lucasmedeiroscosta07@gmail.com>
2023-07-23 12:46:40 -0300

    Cloud

--- persistence/mysql.js
+++ persistence/mysql.js
@@ -1,79 +1,38 @@
 const Sequelize = require('sequelize');
-const fs = require('fs');
-const { promisify } = require('util');
-const waitPort = require('wait-port');
+const fs = require('fs').promises;
 
-const host = HOST_FILE ? fs.readFileSync(HOST_FILE, 'utf8') : HOST;
-  const user = USER_FILE ? fs.readFileSync(USER_FILE, 'utf8') : USER;
-  const password = PASSWORD_FILE
-    ? fs.readFileSync(PASSWORD_FILE, 'utf8')
-    : PASSWORD;
-  const database = DB_FILE ? fs.readFileSync(DB_FILE, 'utf8') : DB;
-  const port = Number(PORT) || 3306;
-const {
-  MYSQL_HOST: HOST,
-  MYSQL_HOST_FILE: HOST_FILE,
-  MYSQL_USER: USER,
-  MYSQL_USER_FILE: USER_FILE,
-  MYSQL_PASSWORD: PASSWORD,
-  MYSQL_PASSWORD_FILE: PASSWORD_FILE,
-  MYSQL_DB: DB,
-  MYSQL_DB_FILE: DB_FILE,
-  MYSQL_PORT: PORT,
-} = process.env;
+const getEnvVar = async (key) => {
+  try {
+    const data = await fs.readFile(`/run/secrets/${key}`, 'utf8');
+    return data.trim();
+  } catch (err) {
+    console.error(`Error reading ${key} from CapRover secrets:`, err);
+    process.exit(1);
+  }
+};
 
+const connectToDatabase = async () => {
+  const host = await getEnvVar('MYSQL_HOST');
+  const user = await getEnvVar('MYSQL_USER');
+  const password = await getEnvVar('MYSQL_PASSWORD');
+  const database = await getEnvVar('MYSQL_DB');
+  const port = Number(await getEnvVar('MYSQL_PORT')) || 3306;
 
-const sequelize = new Sequelize(
-  await waitPort({ host, port }),
-  MYSQL_HOST,
-  MYSQL_USER,
-  MYSQL_PASSWORD,
-  {
+  const sequelize = new Sequelize(database, user, password, {
+    host,
+    port,
     dialect: 'mysql',
-    host: MYSQL_HOST,
-    port: DATABASE_PORT,
-    define: {
-      // Ativa os timestamps e configura para ser criado com
-      // underline entre as palavras.
-      // Docs: https://sequelize.org/docs/v6/core-concepts/model-basics/#timestamps
-      timestamps: true,
-      underscored: true,
-    },
-  },
-);
-try {
-  await sequelize.authenticate();
-  console.log('Connected to MySQL database.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-  process.exit(1);
-}
+  });
 
+  try {
+    await sequelize.authenticate();
+    console.log('Connected to the MySQL database!');
+  } catch (error) {
+    console.error('Error connecting to the database:', error.message);
+    process.exit(1);
+  }
 
-//   // Define a model for the 'todo_items' table
-//   const TodoItem = sequelize.define('todo_items', {
-//     id: {
-//       type: Sequelize.STRING(36),
-//       primaryKey: true,
-//       allowNull: false,
-//     },
-//     name: {
-//       type: Sequelize.STRING(255),
-//       allowNull: false,
-//     },
-//     completed: {
-//       type: Sequelize.BOOLEAN,
-//       allowNull: false,
-//     },
-//   });
+  return sequelize;
+};
 
-//   // Synchronize the model with the database and create the table if not exists
-//   await sequelize.sync();
-// }
-
-// async function teardown() {
-//   await sequelize.close();
-//   console.log('Connection closed.');
-// }
-
-export default sequelize;
+export default connectToDatabase;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const gym_db = require('../persistence/mysql');
+const connectToDatabase = require('../database/sequelize');
+
+
 
 const router = express.Router();
 
@@ -9,7 +11,7 @@ router.get('/', (req, res) => {
 
 router.get('/healthcheck', async (req, res) => {
   try {
-    await gym_db.authenticate();
+    await connectToDatabase.authenticate();
     res.status(204).send();
     console.log('Deu conexão');
   } catch (error) {
@@ -17,5 +19,37 @@ router.get('/healthcheck', async (req, res) => {
     res.status(500).send();
   }
 });
+
+async function main() {
+  try {
+    const sequelize = await connectToDatabase();
+
+    // Seu código com Sequelize aqui
+
+    // Exemplo de uso: obter todos os itens da tabela "todo_items"
+    const TodoItem = sequelize.define('todo_items', {
+      id: {
+        type: Sequelize.STRING(36),
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      completed: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+    });
+
+    const allItems = await TodoItem.findAll();
+    console.log('All items:', allItems.map((item) => item.toJSON()));
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
+
+main();
 
 module.exports = router;
