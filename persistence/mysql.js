597c6f7606092797d82eb8e50539ce687f3c58d0
Lucas Medeiros <lucasmedeiroscosta07@gmail.com>
2023-07-23 11:34:17 -0300

    teste

--- persistence/mysql.js
+++ persistence/mysql.js
@@ -3,6 +3,13 @@ const fs = require('fs');
 const { promisify } = require('util');
 const waitPort = require('wait-port');
 
+const host = HOST_FILE ? fs.readFileSync(HOST_FILE, 'utf8') : HOST;
+  const user = USER_FILE ? fs.readFileSync(USER_FILE, 'utf8') : USER;
+  const password = PASSWORD_FILE
+    ? fs.readFileSync(PASSWORD_FILE, 'utf8')
+    : PASSWORD;
+  const database = DB_FILE ? fs.readFileSync(DB_FILE, 'utf8') : DB;
+  const port = Number(PORT) || 3306;
 const {
   MYSQL_HOST: HOST,
   MYSQL_HOST_FILE: HOST_FILE,
@@ -15,60 +22,58 @@ const {
   MYSQL_PORT: PORT,
 } = process.env;
 
-let sequelize;
 
-async function init() {
-  const host = HOST_FILE ? fs.readFileSync(HOST_FILE, 'utf8') : HOST;
-  const user = USER_FILE ? fs.readFileSync(USER_FILE, 'utf8') : USER;
-  const password = PASSWORD_FILE
-    ? fs.readFileSync(PASSWORD_FILE, 'utf8')
-    : PASSWORD;
-  const database = DB_FILE ? fs.readFileSync(DB_FILE, 'utf8') : DB;
-  const port = Number(PORT) || 3306;
-
-  await waitPort({ host, port });
-
-  sequelize = new Sequelize(database, user, password, {
-    host,
-    port,
+const sequelize = new Sequelize(
+  await waitPort({ host, port }),
+  MYSQL_HOST,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  {
     dialect: 'mysql',
-  });
-
-  try {
-    await sequelize.authenticate();
-    console.log('Connected to MySQL database.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-    process.exit(1);
-  }
-
-  // Define a model for the 'todo_items' table
-  const TodoItem = sequelize.define('todo_items', {
-    id: {
-      type: Sequelize.STRING(36),
-      primaryKey: true,
-      allowNull: false,
+    host: MYSQL_HOST,
+    port: DATABASE_PORT,
+    define: {
+      // Ativa os timestamps e configura para ser criado com
+      // underline entre as palavras.
+      // Docs: https://sequelize.org/docs/v6/core-concepts/model-basics/#timestamps
+      timestamps: true,
+      underscored: true,
     },
-    name: {
-      type: Sequelize.STRING(255),
-      allowNull: false,
-    },
-    completed: {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-    },
-  });
-
-  // Synchronize the model with the database and create the table if not exists
-  await sequelize.sync();
+  },
+);
+try {
+  await sequelize.authenticate();
+  console.log('Connected to MySQL database.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+  process.exit(1);
 }
 
-async function teardown() {
-  await sequelize.close();
-  console.log('Connection closed.');
-}
 
-module.exports = {
-  init,
-  teardown,
-};
+//   // Define a model for the 'todo_items' table
+//   const TodoItem = sequelize.define('todo_items', {
+//     id: {
+//       type: Sequelize.STRING(36),
+//       primaryKey: true,
+//       allowNull: false,
+//     },
+//     name: {
+//       type: Sequelize.STRING(255),
+//       allowNull: false,
+//     },
+//     completed: {
+//       type: Sequelize.BOOLEAN,
+//       allowNull: false,
+//     },
+//   });
+
+//   // Synchronize the model with the database and create the table if not exists
+//   await sequelize.sync();
+// }
+
+// async function teardown() {
+//   await sequelize.close();
+//   console.log('Connection closed.');
+// }
+
+export default sequelize;
